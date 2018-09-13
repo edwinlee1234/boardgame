7e5ab3f44355188256d7214c9fef000bc6231a9f
edwin_lee <edwin_lee@mail.chungyo.net>
2018-09-13 15:27:31 +0800

    新增error package

---
+++ error/main.go
@@ -0,0 +1,29 @@
+package error
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+// ErrorRespone 直接回傳api錯誤信息
+func ErrorRespone(err error, errorCode string, w http.ResponseWriter, statusCode int) bool {
+	if err != nil {
+		var res Response
+		res.Status = "error"
+		res.Data = map[string][]interface{}{}
+		res.Error = map[string]interface{}{}
+
+		res.Error["message"] = err.Error()
+		res.Error["error_code"] = errorCode
+
+		w.WriteHeader(statusCode)
+		log.Println(err)
+
+		json.NewEncoder(w).Encode(res)
+
+		return true
+	}
+
+	return false
+}
